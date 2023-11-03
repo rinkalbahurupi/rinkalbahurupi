5612b93ffe7300bf3be01cc56657a9309427b29c
rinkalbahurupi <149785936+rinkalbahurupi@users.noreply.github.com>
2023-11-03 16:24:04 +0530

    Add files via upload

---
+++ p.py
@@ -0,0 +1,117 @@
+#cost_price = int(input("Enter the cost price:"))
+#selling_price = int(input("Enter the selling price:"))
+
+#if selling_price > cost_price:
+ #   profit = selling_price - cost_price
+  #  print("We have made a profit of", profit)
+#else if : selling_price<cost_price:
+ #   loss = cost_price - selling_price
+  #  print("We have incurred a loss of", loss)
+#else:
+#print("we made have no profit or no loss")
+
+'''number1 = int(input("Enter the number1"))
+number2 = int(input("Enter the number2"))
+sum = number1+number2
+print ("The sum of these two number is",sum)'''
+'''
+arithmatics oprators
+print("sum:", 4+6)
+print("difference:",6-3)
+print("product:",3*5)
+print("floor division:",4//2)
+print("reminder:",4%2)
+print("exponentiation:",4**2)'''
+
+'''assignment oprators
+n1=4
+n2=n1
+print(n1,n2)
+n2*=n1
+print(n1,n2)'''
+
+'''comparison oprators
+n1 = 3
+n2 = 6
+print(n1>n2)'''
+
+
+'''logical oprators
+n = 2 > 1
+g = 3 < 7
+print(n and g)
+print(n or g)
+print(not n)'''
+
+'''identity oprators
+x = 5
+t = 3
+print("if x is t:",x is t)
+print ("if x is not t:",x is not t)'''
+
+'''embership orators
+fruits = "apple", "chiku"
+print("if apple is present in fruits:", "apple" in fruits)
+print("if mango is not present in fruits:", "mango" not in fruits)'''
+
+a = 3
+b = 7
+print(" a and b:", a & b)
+print("a or b:", a | b)
+print(" a xor b:", a ^ b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
